55ae648f0be9f1b1324f4e661aa76b8e127b6f12
Sebastian Lackner <sebastian@fds-team.de>
2013-09-08 22:41:08 +0200

    Removed patches from /wine-patches/ directory, added README files, added patch downloader script

---
+++ wine-patches/get-wine-patches.sh
@@ -0,0 +1,146 @@
+#!/usr/bin/env bash
+OUTPUTDIR="./"
+FORCE=""
+
+usage ()
+{
+	echo ""
+	echo "Usage : ./get-wine-patches.sh [--out=DIRECTORY] [--force]"
+	echo ""
+	echo "This script downloads the wine-patches included in the last wine-compholio"
+	echo "release to the current working directory."
+	echo ""
+}
+
+while [[ $# > 0 ]] ; do
+	case "$1" in
+		--out=*)
+			OUTPUTDIR=${1#*=}
+			;;
+		--out)
+			shift
+			OUTPUTDIR=$1
+			;;
+		--force)
+			FORCE="true"
+			;;
+		--help)
+			usage
+			exit
+			;;
+		*)
+			echo "Error: unknown argument $1" >&2
+			exit 1
+			;;
+	esac
+	shift
+done
+
+# Ensure that the output directory exists
+if [ ! -d "$OUTPUTDIR" ]; then
+	echo "Error: output directory $OUTPUTDIR doesn't exist" >&2
+	exit 1
+fi
+
+# Ensure that there are no other patches in the output directory
+NUMPATCHES=$(find "$OUTPUTDIR" -type f -iname "*.patch" | wc -l)
+if [ "$NUMPATCHES" -ne 0 ] && [ "$FORCE" = "" ]; then
+	echo "Error: output directory already contains some patch files or specify --force" >&2
+	exit 1
+fi
+
+# Get latest release version of wine-compholio
+echo -n " * Checking for last wine-compholio release ... "
+LATESTRELEASE=$(wget -O - -o /dev/null -- "http://ppa.launchpad.net/ehoover/compholio/ubuntu/dists/saucy/main/source/Sources" | \
+	grep -A100 "^Package: *wine-compholio$" | grep "^Version:" | head -n1 | cut -d" " -f2-)
+RET=$?
+if [ "$RET" -ne 0 ] || [ "$LATESTRELEASE" = "" ]; then
+	echo "failed!"
+	echo ""
+	echo "Error: wget returned exitcode $RET" >&2
+	exit 1
+fi
+echo "$LATESTRELEASE"
+
+# Create a temp directory for extracting the patch
+TEMPDIR=$(mktemp -d)
+if [ $? -ne 0 ] || [ "$TEMPDIR" = "" ] || [ ! -d "$TEMPDIR" ]; then
+	echo ""
+	echo "Error: failed to create temp directory" >&2
+	exit 1
+fi
+
+# Delete directory on exit automatically
+trap 'rm -rf "$TEMPDIR"' EXIT
+
+# Find the source DIFF tarball corresponding to the version
+echo -n " * Downloading wine-compholio_${LATESTRELEASE}.diff.gz ... "
+DIFFFILE="$TEMPDIR/diff"
+wget -O - -o /dev/null -- "https://launchpad.net/~ehoover/+archive/compholio/+files/wine-compholio_${LATESTRELEASE}.diff.gz" | \
+	gzip -d --stdout > "$DIFFFILE"
+RET=$?
+if [ "$RET" -ne 0 ] || [ ! -f "$DIFFFILE" ] || [ ! -s "$DIFFFILE" ]; then
+	echo "failed!"
+	echo ""
+	echo "Error: wget returned exitcode $RET" >&2
+	exit 1
+fi
+echo "done"
+
+# Only extract the wine-patches
+echo -n " * Extracting wine patches ... "
+patch -p1 -d "$TEMPDIR/" < <(filterdiff -z -i '[^/]*/patches/*' "$DIFFFILE") &> /dev/null
+RET=$?
+if [ "$RET" -ne 0 ]; then
+	echo "failed!"
+	echo ""
+	echo "Error: patch failed with exitcode $RET" >&2
+	exit 1
+fi
+echo "done"
+
+# Copy the files
+echo -n " * Copying patches to output directory ... "
+RET=0
+echo -n "" > "$TEMPDIR/SHA256SUMS"
+while read FILENAME; do
+	cp "$TEMPDIR/patches/$FILENAME" "$OUTPUTDIR" 2> /dev/null
+	if [ $? -ne 0 ]; then
+		RET=$?
+	fi
+	SHA256=$(sha256sum "$TEMPDIR/patches/$FILENAME" | cut -d' ' -f1)
+	echo "$SHA256  $FILENAME" >> "$TEMPDIR/SHA256SUMS"
+done < <(ls "$TEMPDIR/patches")
+
+if [ "$RET" -ne 0 ]; then
+	echo "failed!"
+	echo ""
+	echo "Error: copy failed with exitcode $RET" >&2
+	exit 1
+fi
+echo "done"
+echo ""
+
+# Display the result o the user if everything was successful
+echo "The following patches have been installed to $OUTPUTDIR:"
+while read FILENAME; do
+	echo " - $FILENAME"
+done < <(ls "$TEMPDIR/patches")
+echo ""
+
+# Verify checksums
+LATESTRELEASECLEAN=$(echo "$LATESTRELEASE" | cut -d"~" -f1)
+if [ -f "$OUTPUTDIR/SHA256SUMS-$LATESTRELEASECLEAN" ]; then
+	diff "$TEMPDIR/SHA256SUMS" "$OUTPUTDIR/SHA256SUMS-$LATESTRELEASECLEAN" &> /dev/null
+	RET="$?"
+	if [ "$RET" -ne 0 ]; then
+		echo "Error: extracted patches don't match SHA256SUMS-$LATESTRELEASECLEAN file" >&2
+		exit 1
+	fi
+	echo "Checksums okay!"
+else
+	echo "Warning: No SHA256SUMS-$LATESTRELEASECLEAN file found, unable to validate files" >&2
+fi
+
+# Temp directory will be removed automatically by the trap
+exit 0
