5066080427e1880b5e155c7f62a7afe6866c6789
Sebastian Lackner <sebastian@fds-team.de>
2014-03-09 21:24:36 +0100

    Fix compilation with wineg++

--- src/common/common.h
+++ src/common/common.h
@@ -12,14 +12,14 @@
 	#include <io.h>
 #endif
 
-#if defined(__linux__)
+#if defined(__WIN32__)
+	#define XP_WIN 1
+#elif defined(__linux__)
 	#define XP_UNIX 1
 	#define MOZ_X11 1
 #elif defined(__APPLE__)
 	#define XP_UNIX 1
 	#define XP_MACOSX 1
-#elif defined(__WIN32__)
-	#define XP_WIN 1
 #else
 	#error "Could not detect your operating system!"
 #endif
